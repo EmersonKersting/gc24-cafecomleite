721ae138dae33fbe8a694a25366526df88543c29
NiruJaguara <callmenanda20@gmail.com>
2024-11-25 20:37:25 -0300

    creates ZeldaGatewayService

---
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/services/ZeldaGatewayService.java
@@ -0,0 +1,14 @@
+package com.caldeira.projetofinal.zelda.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ZeldaGatewayService {
+
+    private final RestTemplate restTemplate;
+
+    public ZeldaGatewayService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+}
