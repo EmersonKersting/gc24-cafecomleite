4f0cb5777d2a0e5b1b0b608af91911cb0b8ecad5
lucas <lucasmroza2003@gmail.com>
2024-11-29 12:50:00 -0300

    Updating ZeldaController (removing AllArgsConstructor)

--- zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
@@ -2,12 +2,12 @@ package com.caldeira.projetofinal.zelda.cotrollers;
 
 @RestController
 @RequestMapping("/zelda")
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ZeldaController {
 
+    @Autowired
     private final ZeldaGetawayService zeldaGetawayService;
 
-    @Autowired
+
     public ZeldaController(ZeldaController zeldaController){
         this.zeldaGetawayService = zeldaController.zeldaGetawayService;
     }
