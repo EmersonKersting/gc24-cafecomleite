0159c740813c664af63c265d5bfc38a73652d71b
Tiago <tiagobackeseicheich@gmail.com>
2024-11-29 17:53:39 -0300

    create GetAll method

--- zelda/src/main/java/com/caldeira/projetofinal/zelda/services/ZeldaGatewayService.java
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/services/ZeldaGatewayService.java
@@ -1,11 +1,13 @@
 package com.caldeira.projetofinal.zelda.services;
 
-import com.caldeira.projetofinal.zelda.models.GameModel;
 import com.caldeira.projetofinal.zelda.models.GameListResponseModel;
+import com.caldeira.projetofinal.zelda.models.GameModel;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+
 
 @Service
 public class ZeldaGatewayService {
@@ -14,6 +16,21 @@ public class ZeldaGatewayService {
 
     public ZeldaGatewayService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    public List<GameModel> getAll(Integer page, Integer size) {
+        int defaultPage = (page != null) ? page : 0;
+        int defaultSize = (size != null) ? size : 5;
+
+        String url = UriComponentsBuilder
+                .fromHttpUrl("https://zelda.fanapis.com/api/games")
+                .queryParam("limit", defaultSize)
+                .queryParam("page", defaultPage)
+                .toUriString();
+
+        GameListResponseModel response = restTemplate.getForObject(url, GameListResponseModel.class);
+
+        return response != null ? response.getData() : List.of();
     }
 
 }
