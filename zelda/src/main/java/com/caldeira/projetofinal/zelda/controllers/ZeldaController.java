193057b312969fec6792f4f8360133b55f886a08
Tiago Eich <tiagobackeseicheich@gmail.com>
2024-12-01 19:25:14 -0300

    creates method to retrieve user by id

--- zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
@@ -1,2 +1,40 @@
-package com.caldeira.projetofinal.zelda.controllers;public class ZeldaController {
+package com.caldeira.projetofinal.zelda.controllers;
+
+import com.caldeira.projetofinal.zelda.models.GameModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.caldeira.projetofinal.zelda.services.ZeldaGatewayService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/zelda")
+public class ZeldaController{
+
+    private final ZeldaGatewayService zeldaGatewayService;
+
+    public ZeldaController(ZeldaGatewayService zeldaGatewayService) {
+        this.zeldaGatewayService = zeldaGatewayService;
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<GameModel>> getAll(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size) {
+        List<GameModel> games = zeldaGatewayService.getAll(page, size);
+        return ResponseEntity.ok(games);
+    }
+
+    @GetMapping("/get-by-id/{id}")
+    public ResponseEntity<GameModel> getById(@PathVariable String id) {
+        GameModel game = zeldaGatewayService.getById(id);
+
+        if (game == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(game);
+    }
+
+
 }
