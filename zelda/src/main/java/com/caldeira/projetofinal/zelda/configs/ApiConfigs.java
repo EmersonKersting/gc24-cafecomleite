d8bddd51fff1bfb09e419f051329ab8a67b42a2f
Tiago Eich <tiagobackeseicheich@gmail.com>
2024-12-02 18:47:59 -0300

    fixes api configuration

--- zelda/src/main/java/com/caldeira/projetofinal/zelda/configs/ApiConfigs.java
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/configs/ApiConfigs.java
@@ -1,4 +1,14 @@
 package com.caldeira.projetofinal.zelda.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class ApiConfigs {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
